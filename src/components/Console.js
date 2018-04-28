6ae853b9e81cf5b10157c6c4336fba7b97c37aa0
Ryan <>
2018-04-28 12:05:55 -0700

    Move the bulk of the components out of the App.js file

--- src/components/Console.js
+++ src/components/Console.js
@@ -3,24 +3,26 @@ import { inject } from 'mobx-react'
 
 class FreeCell extends Component {
   render() {
+    const { store } = this.props;
     const componentStyles = {
       height: '100%',
-    }
+      width: '100%',
+      padding: '25px',
+    };
 
     const textAreaStyles = {
       width: '100%',
-      height: '100%',
+      height: '300px',
       backgroundColor: 'lightgray',
     };
 
-    const { store } = this.props;
     return (
-      <div className="consoleComponent" style={componentStyles}>
-        <div className="consoleHistory">history</div>
-        <textarea className="console" onChange={this.handleConsoleChange.bind(this)} style={textAreaStyles}></textarea>
-        <button className="consoleButton" onClick={() => {store.runConsoleCommands()}}>Run That Shit</button>
+      <div className="console" style={componentStyles}>
+        <div className="history">history</div>
+        <textarea className="command" onChange={this.handleConsoleChange.bind(this)} style={textAreaStyles}></textarea>
+        <button className="submit" onClick={() => {store.runConsoleCommands()}}>Run That Shit</button>
       </div>
-    )
+    );
   }
 
   handleConsoleChange(e) {
