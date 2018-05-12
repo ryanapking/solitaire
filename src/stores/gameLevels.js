c3a0c3c81e0edd95f6b83eb2ae4b17e8c9c8470b
Ryan Apking <ryan.apking@gmail.com>
2018-05-12 10:55:44 -0700

    Add a few basic win condition check functions and update the checkPlayedCardStack function to work with full stacks.

--- src/stores/gameLevels.js
+++ src/stores/gameLevels.js
@@ -1,5 +1,7 @@
 const allMethods = ["autoPlay", "dropCards", "grabCards", "placeCards", "removeCards", "moveCards", "exportGameState", "switchLevels"];
 
+const wonGameState = {"columns":[[],[],[],[],[],[],[],[]],"freeCells":[null,null,null,null],"playedCards":[["21","22","23","24","25","26","27","28","29","210","211","212","213"],["31","32","33","34","35","36","37","38","39","310","311","312","313"],["11","12","13","14","15","16","17","18","19","110","111","112","113"],["41","42","43","44","45","46","47","48","49","410","411","412","413"]]};
+
 export default {
   1: {
     level: 1,
