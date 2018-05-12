46cbb6a0f40b0ea9093820123a0c8032c42f45a3
Ryan Apking <ryan.apking@gmail.com>
2018-05-12 16:39:41 -0700

    Use codemirror to style the console.

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
 
+import {Controlled as CodeMirror} from 'react-codemirror2'
+
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+
 class FreeCell extends Component {
   render() {
     const { store } = this.props;
 
+    const codemirrorOptions = {
+      mode: 'javascript',
+      theme: 'dracula',
+      lineNumbers: true,
+    };
+
     return (
       <div className="console">
         <textarea className="history"value={store.consoleHistory} />
-        <textarea className="command" onChange={this.handleConsoleChange.bind(this)} value={store.consoleCommand} />
+        <CodeMirror value={store.consoleCommand} options={codemirrorOptions}
+          onBeforeChange={(editor, data, value) => {
+            this.handleConsoleChange(value);
+          }}
+        />
         <button className="submit" onClick={this.submitCommand.bind(this)}>Run That Shit</button>
       </div>
     );
   }
 
-  handleConsoleChange(e) {
-    this.props.store.consoleCommand = e.target.value;
+  handleConsoleChange(value) {
+    this.props.store.consoleCommand = value;
   }
 
   submitCommand() {
